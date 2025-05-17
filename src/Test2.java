5b8691c9365dede58f0f0e2e763d89cf9f9f1601
Ambreshwer <ambreshwer95@gmail.com>
2025-05-17 21:43:45 +0530

    2nd from amb

--- src/Test2.java
+++ src/Test2.java
@@ -3,8 +3,8 @@ public class Test2 {
 
 	public static void main(String[] args) {
 
-		System.out.println("21");
-		System.out.println("22");
+		System.out.println("31");
+		System.out.println("32");
 		
 		
 	}
