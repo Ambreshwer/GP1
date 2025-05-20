8cb324269753f81a1da73c3e4b1071428d8f9552
Ambreshwer <ambreshwer95@gmail.com>
2025-05-20 16:48:36 +0530

    mst chng

--- src/Test1.java
+++ src/Test1.java
@@ -6,6 +6,9 @@ public class Test1 {
 		System.out.println("01");
 		System.out.println("2");
 		System.out.println("3");
+		System.out.println("01");
+		System.out.println("2");
+		System.out.println("3");
 		System.out.println("4");
 		System.out.println("5");
 
